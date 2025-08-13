6be51fadd31fa0ef4aebb8b098201b5b420612b8
Mo R <mubynr@gmail.com>
2025-08-13 20:29:28 +0100

    commit

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,33 @@
+"use client"
 import Image from 'next/image'
 import headshot from '@/public/images/contactPage/headshot.jpeg'
 
 export default function ContactPage () {
+
+
+      async function handleSubmit(event) {
+          event.preventDefault();
+          const formData = new FormData(event.target);
+
+          formData.append("access_key", "c365b7a4-1c1b-4e0b-8a44-418ba071b672");
+
+          const object = Object.fromEntries(formData);
+          const json = JSON.stringify(object);
+
+          const response = await fetch("https://api.web3forms.com/submit", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+              },
+              body: json
+          });
+          const result = await response.json();
+          if (result.success) {
+              console.log(result);
+          }
+      }
+
     return (
         <>
         <div className="w-[50%] ">
@@ -15,6 +41,14 @@ export default function ContactPage () {
         <h2>
             I’m a Florida-based photographer and graphic designer with a passion for visual storytelling. Whether behind the camera or at the design desk, I love bringing creative ideas to life with a personal touch. If you’re interested in working together or just want to connect, I’d love to hear from you.
         </h2>
-        </>
-    )
-}
+      
+
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="name"/>
+          <input type="email" name="email"/>
+          <textarea name="message"></textarea>
+          <button type="submit">Submit Form</button>
+        </form>
+      </>
+    );
+  }
--- app/photography/[id]/page.tsx
+++ app/photography/[id]/page.tsx
@@ -1,11 +1,57 @@
 "use client"
 
-import {useParams} from 'next/navigation';
+import Image from "next/image"
+import { useState } from "react"
+import { useParams } from "next/navigation"
+import Link from "next/link"
 
 export default function PhotographyGallery() {
-    const {id} = useParams();
+    const { id } = useParams()
 
-    return(
-        <div>{id}</div>
-    )
+  const images = [
+    "/images/hero/bigSplash.jpg",
+    "/images/photoWheel/photo3.jpg",
+    "/images/photoWheel/photo5.jpg",
+    "/images/photoWheel/photo4.jpg",
+    "/images/hero/bigSplash.jpg"
+  ]
+
+  return (
+        <>
+        <Link href='/photography' className='absolute pt-5'>Back</Link>
+        <h1 className="text-center p-5 text-2xl">{id}</h1>
+      <div className="flex flex-wrap gap-2 justify-center">
+      {images.map((src, i) => (
+        <ResponsiveImage key={i} src={src} alt={`photo ${i}`} />
+      ))}
+    </div>
+      </>
+  )
 }
+
+type ImageProps = {
+    src: string,
+    alt: string,
+}
+
+function ResponsiveImage({ src, alt }: ImageProps) {
+  const [ratio, setRatio] = useState(4/3) // default square
+
+  return (
+    <div
+      className="relative h-35"
+      style={{ aspectRatio: ratio }}
+    >
+      <Image
+        src={src}
+        alt={alt}
+        fill
+        className="object-cover rounded"
+        onLoadingComplete={(img) =>
+          setRatio(img.naturalWidth / img.naturalHeight)
+        }
+      />
+    </div>
+  )
+}
+``
