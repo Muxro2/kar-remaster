2a28ead7687c0ac606698e0196efc06555f2c402
Mo R <mubynr@gmail.com>
2025-08-16 21:03:17 +0100

    finished styling contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,7 @@
 "use client"
+import Link from 'next/link';
 import Image from 'next/image'
-import headshot from '@/public/images/contactPage/headshot.jpeg'
+import headshot from '@/public/images/contactPage/headshot.jpg'
 
 export default function ContactPage () {
 
@@ -31,7 +32,7 @@ export default function ContactPage () {
     return (
       <>
         <div className="relative h-60 w-full overflow-y-hidden z-[-1]">
-          <h2 className='absolute z-1 w-[40%] right-5 pt-7 font-bold'>
+          <h2 className='absolute z-1 w-[40%] right-5 pt-7 font-semibold tracking-[0.5] drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)]'>
             I’m a Florida-based photographer and designer passionate about bringing creative stories to life.
           </h2>
           <Image
@@ -43,29 +44,62 @@ export default function ContactPage () {
         </div>
 
 
-            <div className="relative w-[50%] mt-[-40%]">
-            <h2 className='absolute top-[45%] right-[-75%] text-6xl font-serif italic'>Karina</h2>
+            <div className="relative w-[50%] mt-[-40%] ml-5 ">
+            <h2 className='absolute top-[50%] right-[-75%] text-6xl font-serif italic drop-shadow-[0_2px_2px_rgba(0,0,0,0.8) z-10'>Karina</h2>
             <Image
                 src={headshot}
                 alt="headshot"
                 width={300}
                 height={500}
+                className='brightness-80'
             />
         </div>
         
       
+      <div className='bg-black pt-15 pb-8 mt-[-40] px-4'>
+          <h1 className='text-4xl font-serif'>Contact</h1>
+          <h2 className='pt-2 pb-4'>Interested in working together? Fill out the form below and I'll get back to you as soon as possible.</h2>
+         
+          <form onSubmit={handleSubmit} className='relative bg-black'>
+         
+          <div className='flex h-10 gap-4'>
+          <input type="text" name="name" placeholder='Name' 
+          className='border-white border-2 w-[40%] pl-3 rounded-lg'
+          />
+          <input type="email" name="email" placeholder='Email Address'
+          className='border-white border-2 w-[60%] pl-3 rounded-lg'
+          />
+          </div>
 
-        <form onSubmit={handleSubmit}>
-          <input type="text" name="name" placeholder='name' 
-          className='border-white border-2'
-          />
-          <input type="email" name="email" placeholder='email'
-          className='border-white border-2'
-          
-          />
-          <textarea name="message" placeholder='message'></textarea>
-          <button type="submit">Submit Form</button>
+          <textarea name="message" placeholder='Write a short message...'
+          className='w-full mt-4 border-white border-2 pl-3 pt-2 rounded-lg' rows={3}></textarea>
+          <button type="submit" className='absolute bottom-5 right-3.5 border-2 w-7 h-7 rounded-lg'>{`>`}</button>
         </form>
+      </div>
+
+      <div className='flex w-full py-8 px-12 gap-12'>
+      <div className='flex-1'>
+        <Link href='https://github.com/kar-03'>
+          <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path fill='#fff' d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"/></svg>
+        </Link>
+      </div>
+      <div className='flex-1'>
+        <Link href='https://www.behance.net/karinaprezeau'>
+          <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Behance</title><path fill='#fff' d="M16.969 16.927a2.561 2.561 0 0 0 1.901.677 2.501 2.501 0 0 0 1.531-.475c.362-.235.636-.584.779-.99h2.585a5.091 5.091 0 0 1-1.9 2.896 5.292 5.292 0 0 1-3.091.88 5.839 5.839 0 0 1-2.284-.433 4.871 4.871 0 0 1-1.723-1.211 5.657 5.657 0 0 1-1.08-1.874 7.057 7.057 0 0 1-.383-2.393c-.005-.8.129-1.595.396-2.349a5.313 5.313 0 0 1 5.088-3.604 4.87 4.87 0 0 1 2.376.563c.661.362 1.231.87 1.668 1.485a6.2 6.2 0 0 1 .943 2.133c.194.821.263 1.666.205 2.508h-7.699c-.063.79.184 1.574.688 2.187ZM6.947 4.084a8.065 8.065 0 0 1 1.928.198 4.29 4.29 0 0 1 1.49.638c.418.303.748.711.958 1.182.241.579.357 1.203.341 1.83a3.506 3.506 0 0 1-.506 1.961 3.726 3.726 0 0 1-1.503 1.287 3.588 3.588 0 0 1 2.027 1.437c.464.747.697 1.615.67 2.494a4.593 4.593 0 0 1-.423 2.032 3.945 3.945 0 0 1-1.163 1.413 5.114 5.114 0 0 1-1.683.807 7.135 7.135 0 0 1-1.928.259H0V4.084h6.947Zm-.235 12.9c.308.004.616-.029.916-.099a2.18 2.18 0 0 0 .766-.332c.228-.158.411-.371.534-.619.142-.317.208-.663.191-1.009a2.08 2.08 0 0 0-.642-1.715 2.618 2.618 0 0 0-1.696-.505h-3.54v4.279h3.471Zm13.635-5.967a2.13 2.13 0 0 0-1.654-.619 2.336 2.336 0 0 0-1.163.259 2.474 2.474 0 0 0-.738.62 2.359 2.359 0 0 0-.396.792c-.074.239-.12.485-.137.734h4.769a3.239 3.239 0 0 0-.679-1.785l-.002-.001Zm-13.813-.648a2.254 2.254 0 0 0 1.423-.433c.399-.355.607-.88.56-1.413a1.916 1.916 0 0 0-.178-.891 1.298 1.298 0 0 0-.495-.533 1.851 1.851 0 0 0-.711-.274 3.966 3.966 0 0 0-.835-.073H3.241v3.631h3.293v-.014ZM21.62 5.122h-5.976v1.527h5.976V5.122Z"/></svg>
+        </Link>
+      </div>
+      <div className='flex-1'>
+        <Link href='https://www.linkedin.com/in/karina-prezeau-9517a7221/'>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><path fill='#fff' d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"></path></svg>
+        </Link>
+      </div>
+      <div className='flex-1'>
+        <Link href='https://www.instagram.com/karinaprezeau/'>
+          <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Instagram</title><path fill='#fff' d="M7.0301.084c-1.2768.0602-2.1487.264-2.911.5634-.7888.3075-1.4575.72-2.1228 1.3877-.6652.6677-1.075 1.3368-1.3802 2.127-.2954.7638-.4956 1.6365-.552 2.914-.0564 1.2775-.0689 1.6882-.0626 4.947.0062 3.2586.0206 3.6671.0825 4.9473.061 1.2765.264 2.1482.5635 2.9107.308.7889.72 1.4573 1.388 2.1228.6679.6655 1.3365 1.0743 2.1285 1.38.7632.295 1.6361.4961 2.9134.552 1.2773.056 1.6884.069 4.9462.0627 3.2578-.0062 3.668-.0207 4.9478-.0814 1.28-.0607 2.147-.2652 2.9098-.5633.7889-.3086 1.4578-.72 2.1228-1.3881.665-.6682 1.0745-1.3378 1.3795-2.1284.2957-.7632.4966-1.636.552-2.9124.056-1.2809.0692-1.6898.063-4.948-.0063-3.2583-.021-3.6668-.0817-4.9465-.0607-1.2797-.264-2.1487-.5633-2.9117-.3084-.7889-.72-1.4568-1.3876-2.1228C21.2982 1.33 20.628.9208 19.8378.6165 19.074.321 18.2017.1197 16.9244.0645 15.6471.0093 15.236-.005 11.977.0014 8.718.0076 8.31.0215 7.0301.0839m.1402 21.6932c-1.17-.0509-1.8053-.2453-2.2287-.408-.5606-.216-.96-.4771-1.3819-.895-.422-.4178-.6811-.8186-.9-1.378-.1644-.4234-.3624-1.058-.4171-2.228-.0595-1.2645-.072-1.6442-.079-4.848-.007-3.2037.0053-3.583.0607-4.848.05-1.169.2456-1.805.408-2.2282.216-.5613.4762-.96.895-1.3816.4188-.4217.8184-.6814 1.3783-.9003.423-.1651 1.0575-.3614 2.227-.4171 1.2655-.06 1.6447-.072 4.848-.079 3.2033-.007 3.5835.005 4.8495.0608 1.169.0508 1.8053.2445 2.228.408.5608.216.96.4754 1.3816.895.4217.4194.6816.8176.9005 1.3787.1653.4217.3617 1.056.4169 2.2263.0602 1.2655.0739 1.645.0796 4.848.0058 3.203-.0055 3.5834-.061 4.848-.051 1.17-.245 1.8055-.408 2.2294-.216.5604-.4763.96-.8954 1.3814-.419.4215-.8181.6811-1.3783.9-.4224.1649-1.0577.3617-2.2262.4174-1.2656.0595-1.6448.072-4.8493.079-3.2045.007-3.5825-.006-4.848-.0608M16.953 5.5864A1.44 1.44 0 1 0 18.39 4.144a1.44 1.44 0 0 0-1.437 1.4424M5.8385 12.012c.0067 3.4032 2.7706 6.1557 6.173 6.1493 3.4026-.0065 6.157-2.7701 6.1506-6.1733-.0065-3.4032-2.771-6.1565-6.174-6.1498-3.403.0067-6.156 2.771-6.1496 6.1738M8 12.0077a4 4 0 1 1 4.008 3.9921A3.9996 3.9996 0 0 1 8 12.0077"/></svg>
+        </Link>
+      </div>
+      </div>
+
       </>
     );
   }
